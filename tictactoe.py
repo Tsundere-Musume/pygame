427bb3798de9bfb62cd720a155f09a4eef9ff96f
Tsundere-Musume <maharjanelvis55@gmail.com>
2024-02-20 20:15:32 -0600

    modified tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -4,8 +4,8 @@ import sys
 
 WINDOW_HEIGHT = 720
 WINDOW_WIDTH = 1280
-BACKGROUND = (0, 0, 0)
-FOREGROUND = (255, 255, 255)
+BACKGROUND = (30, 30, 46)
+FOREGROUND = (242, 205, 205)
 
 
 class Game:
@@ -13,7 +13,6 @@ class Game:
         pygame.init()
         pygame.display.set_caption('Tictactoe')
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        # self.board = [[None, None, None], [None,None,None], [None,None,None]]
         self.board = [None] * 9
         self.gridsize = min(WINDOW_WIDTH, WINDOW_HEIGHT) / 5
         self.gridx = int((WINDOW_WIDTH - 3 * self.gridsize) / 2)
@@ -31,38 +30,31 @@ class Game:
         self.player_2 = 2
         self.current_player = self.player_1
 
-    def draw_circle(self, grid_no, width=5):
-        x, y = self.get_grid_coord(grid_no)
-        centerx = int(x + self.gridsize / 2)
-        centery = y + int(self.gridsize / 2)
-        r = int((self.gridsize - 30) / 2)
-        pygame.gfxdraw.aacircle(self.screen, centerx, centery, r, FOREGROUND)
-        pygame.gfxdraw.filled_circle(
-            self.screen, centerx, centery, r, FOREGROUND
-        )
-        pygame.gfxdraw.aacircle(
-            self.screen, centerx, centery, r - width, BACKGROUND
-        )
-        pygame.gfxdraw.filled_circle(
-            self.screen, centerx, centery, r - width, BACKGROUND
-        )
+    def draw_circle(self, grid_no, width=10, opacity=255):
+        rect = self.rects[grid_no]
+        s = pygame.Surface(rect.size)
+        s.set_colorkey((0, 0, 0))
+        r = int(rect.width / 2 - 1)
+        mid = (int(rect.width / 2), int(rect.height / 2))
+        s.set_alpha(opacity)
+        pygame.gfxdraw.aacircle(s, *mid, r, FOREGROUND)
+        pygame.gfxdraw.filled_circle(s, *mid, r, FOREGROUND)
+        pygame.gfxdraw.aacircle(s, *mid, r - width, BACKGROUND)
+        pygame.gfxdraw.filled_circle(s, *mid, r - width, BACKGROUND)
+        self.screen.blit(s, rect.topleft)
 
-    def draw_cross(self, grid_no, width=5):
-        x, y = self.get_grid_coord(grid_no)
+    def draw_cross(self, grid_no, width=5, opacity=255):
+        rect = self.rects[grid_no]
+        s = pygame.Surface(rect.size)
+        s.set_colorkey((0, 0, 0))
+        s.set_alpha(opacity)
         pygame.draw.line(
-            self.screen,
-            FOREGROUND,
-            (x + 15, y + 15),
-            (x + self.gridsize - 15, y + self.gridsize - 15),
-            width=2 * width,
+            s, FOREGROUND, (0, 0), (rect.width, rect.height), width=2 * width
         )
         pygame.draw.line(
-            self.screen,
-            FOREGROUND,
-            (x + self.gridsize - 15, y + 15),
-            (x + 15, y + self.gridsize - 15),
-            width=2 * width,
+            s, FOREGROUND, (rect.width, 0), (0, rect.height), width=2 * width
         )
+        self.screen.blit(s, rect.topleft)
 
     def get_grid_coord(self, grid_no):
         x = self.gridx + (grid_no % 3) * self.gridsize
@@ -116,16 +108,7 @@ class Game:
                     )
 
     def check_win(self):
-        checks = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ]
+        checks = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
         for c in checks:
             a, b, c = self.board[c[0]], self.board[c[1]], self.board[c[2]]
             if all((a, b, c)) and a == b == c:
@@ -140,10 +123,10 @@ class Game:
             for ind, rect in enumerate(self.rects):
                 hit = rect.collidepoint(pos)
                 if hit and not self.board[ind]:
-                    s = pygame.Surface((rect.width, rect.height))
-                    pygame.draw.rect(
-                        self.screen, (255, 0, 0), rect, border_radius=15
-                    )
+                    if self.current_player == self.player_1:
+                        self.draw_circle(ind, opacity=40)
+                    else:
+                        self.draw_cross(ind, opacity=40)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -155,7 +138,6 @@ class Game:
             self.render_moves()
             if self.check_win():
                 break
-        print(self.current_player)
 
     def render_moves(self):
         for index, move in enumerate(self.board):
